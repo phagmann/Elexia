5081f468a97f26d2b42969bfdd8e7b4c59ef3574
phagmann <phagmann1@gmail.com>
2018-11-04 19:48:47 -0800

    teacher vs student ui fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,29 +21,29 @@ Menu.create(hierarchy: "Teachers", category: "Create", title: "Lesson", order_ca
 User.create(email: "t1@comp.com", first_name: "Patrick", last_name: "Smith" , user_code: "tys9KGgNKO", password: "testtest", password_confirmation: "testtest", is_teacher: true)
 User.create(email: "s1@comp.com", first_name: "Carl", last_name: "Mills" , user_code: "ZJzlDwMaSs", password: "testtest", password_confirmation: "testtest", is_teacher: false)
 User.create(email: "s2@comp.com", first_name: "Jacob", last_name: "Lopez" , user_code: "Q1xXAeGS30", password: "testtest", password_confirmation: "testtest", is_teacher: false)
+User.create(email: "t2@comp.com", first_name: "Jocelyn", last_name: "Smith" , user_code: "jfiP930uq1", password: "testtest", password_confirmation: "testtest", is_teacher: true)
 
 
 Subject.create(teacher_id: 1, subject_name: "Math", subject_description: "The one with numbers!", url_path: nil)
+Subject.create(teacher_id: 4, subject_name: "Math", subject_description: "The one with numbers!", url_path: nil)
 
 Book.create(teacher_id: 1, subject_id: 1, book_name: "Intro to Arithmatic", book_description: "3rd grade level", url_path: '/books/1', book_weights_string_list: '80,20')
-
-
-
-# Book.create(teacher_id: 1, subject_id: 1, book_name: "Advanced Arithmatic", book_description: "4th grade level", url_path: '/books/2')
-# Book.create(teacher_id: 1, subject_id: 1, book_name: "Intro to Varibles", book_description: "5rd grade level", url_path: '/books/3')
-# Book.create(teacher_id: 1, subject_id: 1, book_name: "Advanced Varibles", book_description: "6th grade level", url_path: '/books/4')
+Book.create(teacher_id: 1, subject_id: 1, book_name: "Advanced Arithmatic", book_description: "4th grade level", url_path: '/books/2', book_weights_string_list: '80,20')
+Book.create(teacher_id: 1, subject_id: 1, book_name: "Intro to Varibles", book_description: "5rd grade level", url_path: '/books/3', book_weights_string_list: '80,20')
+Book.create(teacher_id: 4, subject_id: 2, book_name: "Advanced Varibles", book_description: "6th grade level", url_path: '/books/4', book_weights_string_list: '80,20')
 
 Enrollment.create(teacher_id: 1, student_id: 2, subject_id: 1, book_id: 1)
 Enrollment.create(teacher_id: 1, student_id: 3, subject_id: 1, book_id: 1)
-# Enrollment.create(teacher_id: 1, student_id: 2, subject_id: 1, book_id: 2)
-
+Enrollment.create(teacher_id: 1, student_id: 2, subject_id: 1, book_id: 2)
+Enrollment.create(teacher_id: 4, student_id: 1, subject_id: 2, book_id: 4)
 
 Section.create(teacher_id: 1, subject_id: 1, book_id: 1, section_name: "Section 1", section_description: "Addition", url_path: nil)
 # Section.create(teacher_id: 1, subject_id: 1, book_id: 1, section_name: "Section 2", section_description: "Subtraction", url_path: nil)
 # Section.create(teacher_id: 1, subject_id: 1, book_id: 1, section_name: "Section 3", section_description: "Multibly", url_path: nil)
 
 # Section.create(teacher_id: 1, subject_id: 1, book_id: 2, section_name: "Section 1", section_description: "Division", url_path: nil)
-# Section.create(teacher_id: 1, subject_id: 1, book_id: 2, section_name: "Section 2", section_description: "Long Division", url_path: nil)
+
+Section.create(teacher_id: 4, subject_id: 2, book_id: 4, section_name: "Section 1", section_description: "Long Division", url_path: nil)
 
 Chapter.create(teacher_id: 1, subject_id: 1, book_id: 1, section_id: 1, chapter_name: "chpt 1", chapter_description: "Meh", url_path: '/chapters/1')
 # Chapter.create(teacher_id: 1, subject_id: 1, book_id: 1, section_id: 2, chapter_name: "chpt 2", chapter_description: "Meh", url_path: '/chapters/2')
@@ -52,7 +52,7 @@ Chapter.create(teacher_id: 1, subject_id: 1, book_id: 1, section_id: 1, chapter_
 # Chapter.create(teacher_id: 1, subject_id: 1, book_id: 2, section_id: 4, chapter_name: "chpt 5", chapter_description: "Meh", url_path: '/chapters/5')
 # Chapter.create(teacher_id: 1, subject_id: 1, book_id: 2, section_id: 4, chapter_name: "chpt 6", chapter_description: "Meh", url_path: '/chapters/6')
 # Chapter.create(teacher_id: 1, subject_id: 1, book_id: 2, section_id: 4, chapter_name: "chpt 7", chapter_description: "Meh", url_path: '/chapters/7')
-# Chapter.create(teacher_id: 1, subject_id: 1, book_id: 2, section_id: 5, chapter_name: "chpt 8", chapter_description: "Meh", url_path: '/chapters/8')
+Chapter.create(teacher_id: 4, subject_id: 2, book_id: 2, section_id: 2, chapter_name: "Chapter 1", chapter_description: "Meh", url_path: '/chapters/2')
 
 
 
@@ -71,12 +71,16 @@ Grade.create(student_id: 2, book_id: 1, assignment_id: 4, grade_weight_index: 1,
 Grade.create(student_id: 2, book_id: 1, assignment_id: 5, grade_weight_index: 0, order_placement: 3, score_out_of_points: 0.0, teacher_note_for_grade: "INCOMPLETE!")
 
 
-
-
 Lesson.create(teacher_id: 1, subject_id: 1, book_id: 1, section_id: 1, chapter_id: 1, lesson_name: "What are Digits?", lesson_description: "cool stuff!", url_path: nil)
-Box.create(lesson_id: 1, ordering: 2, title: "Example 1", paragraph_id: 1)
+Box.create(lesson_id: 1, ordering: 1, title: "Example 1", paragraph_id: 1)
 Paragraph.create(box_id: 1, note_type: 5, paragraph_text: "This is a simple test of our system. Words should also be searchable via dictionary!")
 Note.create(box_id: 1, user_id: 1, note_type: 5, text: "I am the Walrus")
+
+Lesson.create(teacher_id: 4, subject_id: 2, book_id: 4, section_id: 2, chapter_id: 2, lesson_name: "What is Division?", lesson_description: "cool stuff!", url_path: nil)
+Box.create(lesson_id: 2, ordering: 1, title: "Example 1.5", paragraph_id: 2)
+Paragraph.create(box_id: 2, note_type: 5, paragraph_text: "This is a simple test of our system. Your a nerd")
+Note.create(box_id: 2, user_id: 1, note_type: 5, text: "I am the Walrus Chuu")
+
 # Box.create(lesson_id: 2, ordering: 1, title: "Challenge", written_content: "Lets see you try this! ", expression: "1+167+34+89+452")
 
 # Box.create(lesson_id: 3, ordering: 1, title: "TBA", written_content: "TBA")
